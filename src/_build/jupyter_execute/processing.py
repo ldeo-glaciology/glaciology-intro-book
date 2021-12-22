6ad4311a481636c068c2c8e88895a8920f1eb6eb
Jonathan Kingslake <jkingslake@Jonathans-MacBook-Air.local>
2021-12-22 17:21:30 -0500

    a few new pages and edits

---
+++ src/_build/jupyter_execute/processing.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Processing examples
+# 
+# This notebook contains examples of doing some processing. 
+# 
+# 
+# 
+
+# It can include equations etc.
+# 
+# $
+# \phi_d(t) = \phi_t(t) - \phi_r(t)
+# $
+
+# In[3]:
+
+
+import xarray
+
+
+# In[ ]:
+
+
+
+
